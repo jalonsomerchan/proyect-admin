a2a5241c279a8475502f4e6c9d6c0eb029635f1b
Jorge Alonso <jalonsomerchan@gmail.com>
2026-05-10 11:03:20 +0200

    Configure Astro for Preact and GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,16 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
+import preact from '@astrojs/preact';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jalonsomerchan.github.io',
+  base: '/proyect-admin',
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [mdx(), sitemap()]
+  integrations: [preact(), mdx(), sitemap()]
 });
